34971816424fc1caa53dbf456eb2555810252009
wodn5515 <wodn5515@naver.com>
2021-03-06 16:29:03 +0900

    Add rest_framework to INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restaurant.apps.RestaurantConfig'
+    'restaurant.apps.RestaurantConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
