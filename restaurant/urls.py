6746ef636b9b89f826b634f1d84e6864abc679f7
wodn5515 <wodn5515@naver.com>
2021-03-06 16:22:49 +0900

    Add CBV urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("restaurant", views.restaurant_create_list),
-    path("restaurant/<int:pk>", views.restaurant_detail),
-    path("restaurant/<int:pk>/menu", views.menu_create)
+    # CBV urls
+    path("restaurant", views.RestaurantView.as_view()),
+    path("restaurant/<int:pk>", views.RestaurantDetailView.as_view()),
+    path("restaurant/<int:pk>/menu", views.MenuCreateView.as_view()),
+    # FBV urls
+    #path("restaurant", views.restaurant_create_list),
+    #path("restaurant/<int:pk>", views.restaurant_detail),
+    #path("restaurant/<int:pk>/menu", views.menu_create),
 ]
