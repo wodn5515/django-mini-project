216d7f48947d746293371413043490d0384b8757
wodn5515 <wodn5515@naver.com>
2021-03-06 17:22:28 +0900

    url patterns 분리

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,14 +1,27 @@
-from django.urls import path
+from django.urls import path,include
 
-from . import views
+from . import views, api
 
-urlpatterns = [
-    # CBV urls
+drf_urlpatterns = [
+    path("restaurant", api.RestaurantListCreateView.as_view()),
+    path("restaurant/<int:pk>", api.RestaurantRetrieveView.as_view()),
+    path("restaurant/<int:pk>/menu", api.MenuCreateView.as_view())
+]
+
+cbv_urlpatterns = [
     path("restaurant", views.RestaurantView.as_view()),
     path("restaurant/<int:pk>", views.RestaurantDetailView.as_view()),
-    path("restaurant/<int:pk>/menu", views.MenuCreateView.as_view()),
-    # FBV urls
-    #path("restaurant", views.restaurant_create_list),
-    #path("restaurant/<int:pk>", views.restaurant_detail),
-    #path("restaurant/<int:pk>/menu", views.menu_create),
+    path("restaurant/<int:pk>/menu", views.MenuCreateView.as_view())
+]
+
+fbv_urlpatterns = [
+    path("restaurant", views.restaurant_create_list),
+    path("restaurant/<int:pk>", views.restaurant_detail),
+    path("restaurant/<int:pk>/menu", views.menu_create)
+]
+
+urlpatterns = [
+    path("drf/", include(drf_urlpatterns)),
+    path("cbv/", include(cbv_urlpatterns)),
+    path("fbv/", include(fbv_urlpatterns))
 ]
