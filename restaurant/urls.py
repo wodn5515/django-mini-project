0b49b7e47fd07c923044307819ccfb4876e6e678
wodn5515 <wodn5515@naver.com>
2021-03-06 13:49:58 +0900

    Add restaurant urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path("restaurant", views.restaurant_create_list),
+    path("restaurant/<int:pk>", views.restaurant_detail)
 ]
