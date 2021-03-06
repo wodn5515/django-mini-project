3f5419797b24c9213610772d0eb962ede092c647
wodn5515 <wodn5515@naver.com>
2021-03-06 17:21:50 +0900

    Add api.py
    drf를 이용해 같은 기능 구현

---
+++ restaurant/api.py
@@ -0,0 +1,31 @@
+from rest_framework.generics import ListCreateAPIView, CreateAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import RestaurantListSerializer, RestaurantCreateSerializer, RestaurantDetailSerializer, MenuSerializer
+from .paginations import StandardPagination
+from .models import Restaurant, Menu
+
+class RestaurantListCreateView(ListCreateAPIView):
+    pagination_class = StandardPagination
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return RestaurantListSerializer
+        return RestaurantCreateSerializer
+
+    def get_queryset(self):
+        return Restaurant.objects.values("id", "name", "address", "phone_number").all()
+
+
+class RestaurantRetrieveView(RetrieveAPIView):
+    serializer_class = RestaurantDetailSerializer
+    queryset = Restaurant.objects.all()
+
+
+class MenuCreateView(CreateAPIView):
+    serializer_class = MenuSerializer
+
+    def perform_create(self, serializer):
+        restaurant = Restaurant.objects.get(pk=self.kwargs["pk"])
+        serializer.save(restaurant=restaurant)
