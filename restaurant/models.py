16f450903944f3a9fdfc15497257c68fb072d678
wodn5515 <wodn5515@naver.com>
2021-03-06 12:19:29 +0900

    Add Restaurant model, Menu model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(_("음식점 이름"), max_length=255)
+    description = models.CharField(_("음식점 소개"), max_length=255)
+    address = models.CharField(_("음식점 주소"), max_length=255)
+    phone_number = models.CharField(_("음식점 전화번호"), max_length=255)
+
+    class Meta:
+        db_table = _("restaurant")
+        verbose_name = _("음식점")
+        verbose_name_plural = _("음식점")
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(_("메뉴 이름"), max_length=255)
+    price = models.IntegerField(_("메뉴 가격"))
+
+    class Meta:
+        db_table = _("menu")
+        verbose_name = _("메뉴")
+        verbose_name_plural = _("메뉴")
