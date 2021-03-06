fe9ad3816308cffea4864dd3f10c589e2ed2cfde
wodn5515 <wodn5515@naver.com>
2021-03-06 15:52:14 +0900

    Add menu_create url

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("restaurant", views.restaurant_create_list),
-    path("restaurant/<int:pk>", views.restaurant_detail)
+    path("restaurant/<int:pk>", views.restaurant_detail),
+    path("restaurant/<int:pk>/menu", views.menu_create)
 ]
